926e3e831187f44dd4b8086f84dacd0b8c77223b
Leah Seyoum <leahseyoum@gmail.com>
2023-05-07 21:30:41 -0700

    login

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User');
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
@@ -19,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(passport.initialize());
 // Security Middleware
 if (!isProduction) {
   // Enable CORS only in development because React will be on the React
